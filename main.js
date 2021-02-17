720b59415b02f5e7141a8cb46e2616e311bcbb03
Snishiki <62656782+Snishiki@users.noreply.github.com>
2021-02-17 23:45:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 miro.onReady(() => {
   const icon =
-    '<g id="レイヤー_2" data-name="レイヤー 2"><g id="Rectangle"><rect class="cls-1" width="48" height="48"/></g><g id="icon_data"><rect class="cls-2" x="9" y="6" width="30" height="36"/><line class="cls-2" x1="9" y1="16" x2="33" y2="16"/><circle cx="17" cy="23" r="2"/><circle cx="24" cy="23" r="2"/><circle cx="31" cy="23" r="2"/><circle cx="17" cy="29" r="2"/><circle cx="24" cy="29" r="2"/><circle cx="31" cy="29" r="2"/><circle cx="17" cy="35" r="2"/><circle cx="24" cy="35" r="2"/><circle cx="31" cy="35" r="2"/></g></g>'
+    '<g id="Layer_1"><path d="M2.796,2.468 L2.749,22.468" fill-opacity="0" stroke="#000000" stroke-width="2"/><path d="M3.5,21.5 L23.564,21.5" fill-opacity="0" stroke="#000000" stroke-width="2"/><path d="M2.796,14.783 C3.209,13.972 3.594,13.171 4.059,12.371 C4.623,11.401 5.385,10.556 5.953,9.606 C6.402,8.857 7.01,8.19 7.426,7.395 C7.433,7.382 7.577,7.143 7.584,7.143 C7.752,7.143 8.14,9.507 8.163,9.606 C8.417,10.699 9.623,17.334 10.268,17.95 C10.54,18.21 10.69,17.331 10.846,16.995 C11.092,16.467 11.363,15.957 11.636,15.437 C12.745,13.317 13.466,11.153 14.687,9.053 C15.97,6.848 15.997,5.125 18.107,8.148 C18.173,8.244 19.421,9.809 19.475,9.757 C20.284,8.984 20.992,7.211 21.527,6.188" fill-opacity="0" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></g>'
   miro.initialize({
     extensionPoints: {
       bottomBar: {
